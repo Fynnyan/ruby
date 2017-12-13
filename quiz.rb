6e4c79a26904548cd76735ec4e0c068f5e59e766
Fynnyan <t.z@gmx.ch>
2017-12-13 11:53:08 +0100

    some test

---
+++ quiz.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+
+# stuff
+number1 = 1
+number2 = 1000
+check = false
+
+puts "Hello to the quiz\n\n"
+
+answer = rand(number2)
+puts "please enter a number between #{number1} and #{number2}"
+
+while check == false do
+  question = gets.to_i
+  if question < number2
+    if question > answer
+      puts "your number was to high, guess lower"
+    elsif question == answer
+      puts '(^*-*^)> Correct you WIN!  \[T]/'
+      check = true
+    else
+      puts "your number was to low, guess higher"
+    end
+  else
+    puts "please enter a number lower than #{number2}"
+  end
+end
